f7d3df0e95e393be855d2c0b0ad542de0dc344c7
Juhee <34412320+Hongongong@users.noreply.github.com>
2024-01-02 10:58:48 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    
+	    
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        
+        bw.write("\\    /\\\n");
+        bw.write(" )  ( ')\n");
+        bw.write("(  /  )\n");
+        bw.write(" \\(__)|");
+        
+        // 출력버퍼를 화면에 출력
+        bw.flush();
+
+		// 마무리(관행적으로 필수)
+	    br.close();
+		bw.close();
+		
+		
+	}		
+}
