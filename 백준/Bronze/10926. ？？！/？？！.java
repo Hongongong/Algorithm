0b3fe1778955a8e1e075d54c8e321dc393b3bed7
Juhee <34412320+Hongongong@users.noreply.github.com>
2024-01-16 09:46:30 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+		String id = br.readLine();
+
+		bw.write(id + "??!");
+
+        // 출력버퍼를 화면에 출력
+        bw.flush();
+
+		// 마무리(관행적으로 필수)
+	    br.close();
+		bw.close();
+		
+		
+	}
+}
+
